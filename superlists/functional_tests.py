267e0edc7af399cfe69c6a75556dc3dc749e01ec
Matthew E. O'Hanlon <Mohanlon@integrity-apps.com>
2017-11-15 14:36:47 -0500

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -10,7 +10,7 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
-	def test_can_start_a_list_and_retirieve_it_later(self):
+	def test_can_start_a_list_and_retireive_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
 		self.browser.get('http://localhost:8000')
@@ -39,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table."
 		)
 
 		# There is still a text box inviting her to add another item. She
