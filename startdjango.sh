b5fc16e0beb8def47ba0fe97fb588ca4af4e278b
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-01-26 16:58:09 -0500

    Still getting gunicorn right

--- startdjango.sh
+++ startdjango.sh
@@ -6,6 +6,6 @@ python /superlists/manage.py migrate
 
 python /superlists/manage.py runserver 0.0.0.0:8000 &
 
-cd /superlists && gunicorn superlists.wsgi:application &
+cd /superlists && gunicorn --bind 0.0.0.0:8000 superlists.wsgi:application &
 
 nginx -g "daemon off;"
