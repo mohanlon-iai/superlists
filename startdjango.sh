9d056aa07df80fcbf0eaac0f8bc0c15347f98a53
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-01-30 11:28:54 -0500

    Starting to get a handle on secret key generation for the app.

--- startdjango.sh
+++ startdjango.sh
@@ -4,7 +4,7 @@ python /superlists/vault_client_token.py
 
 python /superlists/manage.py migrate
 
-python /superlists/manage.py runserver 0.0.0.0:8000 &
+#python /superlists/manage.py runserver 0.0.0.0:8000 &
 
 cd /superlists && gunicorn --bind unix:/var/run/gunicorn.socket superlists.wsgi:application &
 
