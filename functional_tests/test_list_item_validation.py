62ed9953b131ff007dc93ad19f46b4efe2db2c48
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-02-01 16:12:40 -0500

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,8 +13,8 @@ class ItemValidationTest(FunctionalTest):
 		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text,
-			"You can't have and empty list item"
+			self.browser.find_element_by_css_selector('.is-invalid').text,
+			"You can't have an empty list item"
 		))
 		# She tries again with some text for the item, which now works
 		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
@@ -26,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
 		
 		# She receives a similar warning on the list page
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text,
+			self.browser.find_element_by_css_selector('.is-invalid').text,
 			"You can't have an empty list item"
 		))
 		
