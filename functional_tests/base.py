325d247832cb0f55fbc47460678127271939c542
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-02-01 14:53:06 -0500

    Splitting up the functional tests into separate files.

---
+++ functional_tests/base.py
@@ -0,0 +1,43 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from decouple import config
+import time
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		staging_server = config('STAGING_SERVER', default='')
+		if staging_server:
+			self.live_server_url = 'http://' + staging_server
+		
+	def tearDown(self):
+		self.browser.refresh()
+		self.browser.quit()
+		
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except(WebDriverException, AssertionError) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e;
+				time.sleep(0.5)
+
+	def wait_for(self, fn):
+		start_time = time.time()
+		
+		while True:
+			try:
+				return fn()
+			except(WebDriverException, AssertionError) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e;
+				time.sleep(0.5)
