3afe6795c0ab6543fe4911745944485828576a43
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-02-01 11:39:19 -0500

    Updated FT to use decouple config rather than os.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,14 +3,15 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-import time, unittest, os
+from decouple import config
+import time, unittest
 
 MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		staging_server = os.environ.get('STAGING_SERVER')
+		staging_server = config('STAGING_SERVER', default='')
 		if staging_server:
 			self.live_server_url = 'http://' + staging_server
 		
