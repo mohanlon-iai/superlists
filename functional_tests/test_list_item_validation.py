a600996e4b230e63a8195c9dc1617a10a44b90fb
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-02-01 16:21:58 -0500

    Needed to change CSS classes on form validation for Bootstrap 4

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,7 +13,7 @@ class ItemValidationTest(FunctionalTest):
 		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.is-invalid').text,
+			self.browser.find_element_by_css_selector('.invalid-feedback').text,
 			"You can't have an empty list item"
 		))
 		# She tries again with some text for the item, which now works
@@ -26,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
 		
 		# She receives a similar warning on the list page
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.is-invalid').text,
+			self.browser.find_element_by_css_selector('.invalid-feedback').text,
 			"You can't have an empty list item"
 		))
 		
