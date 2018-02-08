a2cd5dbeaba4370e551573d64555ffe961a606af
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-02-08 16:16:51 -0500

    Refactored views.py to instantiate an Item object with constructor instead of objects.create method

--- lists/views.py
+++ lists/views.py
@@ -10,23 +10,21 @@ def home_page(request):
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
 	error = None
-	item = None
 	
 	if request.method == 'POST':
 		try:
-			item = Item.objects.create(text=request.POST['item_text'], list=list_)
+			item = Item(text=request.POST['item_text'], list=list_)
 			item.full_clean()
 			item.save()
 			return redirect(list_)
 		except ValidationError:
-			item.delete()
 			error = "You can't have an empty list item"
 			
 	return render(request, 'list.html', {'list': list_, "error": error})
 	
 def new_list(request):
 	list_ = List.objects.create()
-	item = Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
+	item = Item(text=request.POST['item_text'], list=list_)
 	try:
 		item.full_clean()
 		item.save()
