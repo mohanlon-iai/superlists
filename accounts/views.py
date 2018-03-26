88c57c5ceb6f0c7ac102f90870dbb3d83fecd563
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-03-26 14:53:27 -0400

    Created login and email methods in views, added messages to base template, added urls to login and email methods, and added UTs for new views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
 
 # Create your views here.
+
+def send_login_email(request):
+	email = request.POST['email']
+	# print(type(send_mail))
+	send_mail(
+		'Your login link for Superlists',
+		'Use this link to log in', 
+		'noreply@superlists', 
+		[email]
+	)
+	messages.success(
+		request,
+		"Check your email, we've sent you a link you can use to log in."
+	)
+	return redirect('/')
+	
+def login(request):
+	return redirect('/')
