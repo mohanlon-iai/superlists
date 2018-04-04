86d3e50b956598f968cd9fd9d71ec0b93053a87a
Matthew E. O'Hanlon <mohanlon@integrity-apps.com>
2018-04-04 08:49:27 -0400

    Fixed problem with item list validation FT

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,43 +6,88 @@ class ItemValidationTest(FunctionalTest):
 	def get_error_element(self):
 		return self.browser.find_element_by_css_selector('.invalid-feedback')
 
+	def click_and_send_keys(self, element_id, keys_to_send):
+		element_id.click()
+		element_id.send_keys(keys_to_send)
+
 	def test_cannot_add_empty_list_items(self):
 		# Edith goes to the home page and accidentally tries to submit
 		# an empty list item. She hits Enter on the empty input box.
 		self.browser.get(self.live_server_url)
-		self.get_item_input_box().send_keys(Keys.ENTER)
-		
+		self.wait_for(
+			lambda: self.click_and_send_keys(
+				self.browser.find_element_by_id('id_text'), 
+				Keys.ENTER
+			)
+		)
+
 		# The browser intercepts the request, and does not load the 
 		# list page
+		print('looking for invalid pseudo element')
 		self.wait_for(lambda: self.browser.find_element_by_css_selector(
 			'#id_text:invalid'
 		))
-		
+
 		# She starts typing some text for the new item and the error disappears
-		self.get_item_input_box().send_keys('Buy milk')
-		self.wait_for(lambda: self.browser.find_element_by_css_selector(
-			'#id_text:valid'
-		))
-		
+		self.wait_for(
+			lambda: self.click_and_send_keys(
+				self.browser.find_element_by_id('id_text'), 
+				'Buy milk'
+			)
+		)
+		self.assertEqual(
+			'Buy milk', 
+			self.browser.find_element_by_id('id_text').get_attribute("value")
+		)
+		self.wait_for(
+			lambda: self.browser.find_element_by_css_selector(
+				'#id_text:valid'
+			)
+		)
+
 		# And she can submit it successfully
-		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for(
+			lambda: self.click_and_send_keys(
+				self.browser.find_element_by_id('id_text'), 
+				Keys.ENTER
+			)
+		)
 		self.wait_for_row_in_list_table('1: Buy milk')
-		
+
 		# Perversely, she now decides to submit a second blank list item
-		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for(
+			lambda: self.click_and_send_keys(
+				self.browser.find_element_by_id('id_text'), 
+				Keys.ENTER
+			)
+		)
 		
 		# Again the browser will not comply
 		self.wait_for_row_in_list_table('1: Buy milk')
-		self.wait_for(lambda: self.browser.find_element_by_css_selector(
-			'#id_text:invalid'
-		))
-		
+		self.wait_for(
+			lambda: self.browser.find_element_by_css_selector(
+				'#id_text:invalid'
+			)
+		)
+
 		# And she can correct it by filling some text in
-		self.get_item_input_box().send_keys('Make tea')
-		self.wait_for(lambda: self.browser.find_element_by_css_selector(
-			'#id_text:valid'
-		))
-		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for(
+			lambda: self.click_and_send_keys(
+				self.browser.find_element_by_id('id_text'), 
+				'Make tea'
+			)
+		)
+		self.wait_for(
+			lambda: self.browser.find_element_by_css_selector(
+				'#id_text:valid'
+			)
+		)
+		self.wait_for(
+			lambda: self.click_and_send_keys(
+				self.browser.find_element_by_id('id_text'), 
+				Keys.ENTER
+			)
+		)
 		self.wait_for_row_in_list_table('1: Buy milk')
 		self.wait_for_row_in_list_table('2: Make tea')
 
@@ -62,7 +107,8 @@ class ItemValidationTest(FunctionalTest):
 			self.get_error_element().text,
 			"You've already got this item in your list"
 		))
-		
+	
+	
 	def test_error_messages_are_cleared_on_input(self):
 		# Edith starts a list and causes a validation error:
 		self.browser.get(self.live_server_url)
